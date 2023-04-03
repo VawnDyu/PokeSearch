154033afa4f784bacfa4e55e54dc60c66aa86645
Vonne Dew <74760653+VawnDyu@users.noreply.github.com>
2023-04-03 23:41:22 +0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import axios from "axios";
+
+import "./App.css";
 
 function App() {
+  const [pokemon, setPokemon] = useState(null);
+  const [search, setSearch] = useState("");
+  const [error, setError] = useState("");
+
+  const handleSearch = async (event) => {
+    event.preventDefault();
+    if (search.trim() === "") {
+      setError("Please enter a valid Pokemon name or ID.");
+      setPokemon(null);
+      return;
+    }
+    try {
+      const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${search.toLowerCase()}`);
+      setPokemon(response.data);
+      setError("");
+    } catch (error) {
+      setError("Pokemon not found.");
+      setPokemon(null);
+    }
+    setSearch("");
+  };
+
+  const handleChange = (event) => {
+    setError("");
+    setSearch(event.target.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Pokemon Search</h1>
+      <form onSubmit={handleSearch} className="search-container">
+        <input type="text" placeholder="Search Pokemon..." value={search} onChange={handleChange} />
+      </form>
+      {error && <p className="error">{error}</p>}
+      {pokemon && (
+        <div className="details">
+          <h2 className="name">{pokemon.name}</h2>
+          <img src={`https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${pokemon.id.toString().padStart(3, '0')}.png`} alt={pokemon.name} className="image" />
+          <ul>
+            <li>Height: {pokemon.height / 10} m</li>
+            <li>Weight: {pokemon.weight / 10} kg</li>
+            <li>Base Experience: {pokemon.base_experience}</li>
+          </ul>
+        </div>
+      )}
     </div>
   );
 }
